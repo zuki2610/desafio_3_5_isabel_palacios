2e6b4da8bea7628ca828c119b1b63f1df58bc6b2
Isabel Palacios <issabelpalacios@gmail.com>
2022-12-29 17:01:56 -0300

    ADD: funcion contadora de tareas por dia y parametrizaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,40 +48,46 @@ document.getElementById("p-viernes").innerHTML = viernes;
 }
 
 let showInput = true;
-const showBlock = () => {
+
+const showBlock = (dia) => {
   console.log("showBlock");
   if (showInput == true) {
-    document.getElementById("task1").style.display = "block";
+    document.getElementById(`input-task-${dia}`).style.display = "block";
     showInput = false;
   } else {
-    document.getElementById("task1").style.display = "none";
+    document.getElementById(`input-task-${dia}`).style.display = "none";
     showInput = true;
   }
 };
+
 let contadorTareasLunes = 0;
-const validateInput = (event) => {
-  console.log("validateInput");
-  let input = document.getElementById("task1").value;
+let contadorTareasMartes = 0;
+let contadorTareasMiercoles = 0;
+let contadorTareasJueves = 0;
+let contadorTareasViernes = 0;
+
+const validateInput = (event, dia) => {
+  console.log(`dia ${dia}`);
+  let input = document.getElementById(`input-task-${dia}`).value;
   if (input != "") {
     console.log(event.key);
     if (event.key === "Enter") {
       event.preventDefault();
-      contadorTareasLunes = contadorTareasLunes + 1;
+      const cantidadTareas = contadorPorDia(dia);
       // Trigger the button element with a click
-      const tareaactual = document.getElementById("lunes").innerHTML;
-      document.getElementById("lunes").innerHTML =
-        tareaactual +
-        `  <div class="añadido" id="eliminar-lunes-${contadorTareasLunes}">
-      <span>${input}</span>
-      <p class="sumaresta">
-          <span onclick="eliminar('eliminar-lunes-${contadorTareasLunes}')" class="btn">
-              <i class="fa-solid fa-circle-minus" style="color:rgb(240, 116, 116)"></i>
-          </span>
-          <span onclick="editar()" class="btn">
-              <i class="fa-solid fa-circle-plus" style="color:rgb(192, 227, 255)"></i>
-          </span>
-      </p>
-  </div> `;
+      const tareaactual = document.getElementById(dia).innerHTML;
+      document.getElementById(dia).innerHTML =
+        tareaactual + `<div class="añadido" id="eliminar-${dia}-${cantidadTareas}">
+          <span>${input}</span>
+          <p class="sumaresta">
+            <span onclick="eliminar('eliminar-${dia}-${cantidadTareas}')" class="btn">
+                <i class="fa-solid fa-circle-minus" style="color:rgb(240, 116, 116)"></i>
+            </span>
+            <span onclick="editar()" class="btn">
+                <i class="fa-solid fa-circle-plus" style="color:rgb(192, 227, 255)"></i>
+            </span>
+          </p>
+        </div>`;
     }
   }
 };
@@ -90,5 +96,28 @@ const eliminar = (id) => {
   const idEliminar = document.getElementById(id);
   idEliminar.remove();
 }
+
+const contadorPorDia = (dia) => {
+  if (dia == 'lunes') {
+    contadorTareasLunes = contadorTareasLunes + 1;
+    return contadorTareasLunes;
+  }
+  if (dia == 'martes') {
+    contadorTareasMartes = contadorTareasMartes + 1;
+    return contadorTareasMartes;
+  } 
+  if (dia == 'miercoles') {
+    contadorTareasMiercoles = contadorTareasMiercoles + 1;
+    return contadorTareasMiercoles;
+  }
+  if (dia == 'jueves') {
+    contadorTareasJueves = contadorTareasJueves + 1;
+    return contadorTareasJueves;
+  }
+  if (dia == 'viernes') {
+    contadorTareasViernes = contadorTareasViernes + 1;
+    return contadorTareasViernes;
+  }
+};
 
 inicializarFecha();
